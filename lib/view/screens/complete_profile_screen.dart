130e421cf5f128ff584058badc2e1dacb4978a8f
User <shawon.mas@gmail.com>
2022-09-23 12:15:44 +0600

    complete_profile_screen created

--- lib/view/screens/complete_profile_screen.dart
+++ lib/view/screens/complete_profile_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:somerian_health/view/widget/common_toolbar.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../widget/text_widget.dart';
 
 class CompleteProfileScreen extends StatelessWidget {
   const CompleteProfileScreen({Key? key}) : super(key: key);
@@ -8,7 +11,61 @@ class CompleteProfileScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CommonToolbar(title: 'Complete your profile',),
-      body: SizedBox(),
+      body: Column(
+        children: [
+          Container(
+            color: Colors.grey,
+            width: double.infinity,
+            height: 40.h,
+            child: Padding(
+              padding: const EdgeInsets.all(5.0),
+              child: TextWidget(value: "Please provide basic detalis",size: 14.sp,fontWeight: FontWeight.w700,textColor: Colors.black,),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'First Name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                        const BorderSide(color: Colors.black54, width: 1.0),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+
+                    ),
+
+                  ),
+                ),
+                SizedBox(width: 10,),
+                Expanded(
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                        hintText: 'Last Name',
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      focusedBorder: OutlineInputBorder(
+                        borderSide:
+                        const BorderSide(color: Colors.black54, width: 1.0),
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                    ),
+                  ),
+                ),
+
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
