d1a73671275bc5236de4207b722e95e7619a888f
User <shawon.mas@gmail.com>
2022-10-09 00:06:45 +0600

    specialties menu created

--- lib/view/screens/menu_screens/specialties_menu/doctors_list.dart
+++ lib/view/screens/menu_screens/specialties_menu/doctors_list.dart
@@ -2,8 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
+import '../../../../global/properties.dart';
 import '../../../../services/query_doctors.dart';
 import '../../../widget/common_toolbar.dart';
+import '../../../widget/general_button.dart';
+import '../../../widget/text_widget.dart';
+import '../doctors_menu_screens/time_date.dart';
 class DoctorsList extends StatefulWidget {
   final String speciality;
   const DoctorsList({Key? key, required this.speciality}) : super(key: key);
@@ -13,17 +17,26 @@ class DoctorsList extends StatefulWidget {
 }
 
 class _DoctorsListState extends State<DoctorsList> {
+  final CollectionReference _doctors=FirebaseFirestore.instance.collection('doctors');
 
  var doctors;
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    QueryDoctors().getDoctors(widget.speciality)
+  /*  QueryDoctors().getDoctors(widget.speciality)
     .then((QuerySnapshot docs){
-         doctors=docs.docs[0].data();
-         print(doctors);
-    });
+      for(var doc in docs.docs){
+
+        String name = doc.get('name');
+        print(name);
+      }
+      *//*   doctors=docs.docs[0].data();
+         print(doctors);*//*
+    });*/
+
+
+
   }
 
   @override
@@ -42,6 +55,72 @@ class _DoctorsListState extends State<DoctorsList> {
               ],
             ),
           ),
+          Expanded(
+            child: StreamBuilder(
+                stream: _doctors.where('title',isEqualTo:widget.speciality).snapshots(),
+                builder:(context,AsyncSnapshot<QuerySnapshot> snapshot){
+                  if(snapshot.hasData){
+                    return ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context,index){
+                          final DocumentSnapshot documentSnapshot=snapshot.data!.docs[index];
+                          return Padding(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            child: Column(
+                              children: [
+                                Row(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Image.network(documentSnapshot['image'],height:100,width: 100,fit: BoxFit.cover,),
+                                    Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      children: [
+                                        TextWidget(
+                                          value: documentSnapshot['name'],
+                                          size: 18.sp,
+                                          fontWeight: FontWeight.w700,
+                                          textColor: Properties.fontColor,
+                                        ),
+                                        TextWidget(
+                                          value: documentSnapshot['title'],
+                                          size: 14.sp,
+                                          fontWeight: FontWeight.w500,
+                                          textColor: Properties.fontColor,
+                                        ),
+                                        TextWidget(
+                                          value: documentSnapshot['location'],
+                                          size: 14.sp,
+                                          fontWeight: FontWeight.w500,
+                                          textColor: Properties.fontColor,
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                                AppointmentButton(
+                                  onPressed: (){
+                                    Navigator.push(context, MaterialPageRoute(builder: (context)=>TimeDateScreen(
+                                      title: documentSnapshot['title'],image: documentSnapshot['image'],location: documentSnapshot['location'],name: documentSnapshot['name'],
+                                    )));
+                                  },
+                                  value:'Book an Appointment' ,
+                                ),
+
+                                Divider(
+                                  color: Colors.black54,
+                                )
+                              ],
+                            ),
+                          );
+                        }
+                    );
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+
+                }),
+          )
 
         ],
       ),
