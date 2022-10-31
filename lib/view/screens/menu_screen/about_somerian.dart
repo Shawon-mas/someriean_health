fe76ff23461410de08ff38648d6081ea3d85fe23
bihim <zubaerbihim@gmail.com>
2022-10-31 21:29:36 +0600

    Updated about somerian

--- lib/view/screens/menu_screen/about_somerian.dart
+++ lib/view/screens/menu_screen/about_somerian.dart
@@ -5,27 +5,35 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../global/properties.dart';
 import '../../widget/common_toolbar.dart';
 import '../../widget/text_widget.dart';
+
 class AboutSomerian extends StatelessWidget {
   const AboutSomerian({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    //var date="<p>While the nation was fighting the virus outbreak,</p><p>Somerian Health as a volunteer healthcare body came</p><p>forward to join forces with the civic body.</p><p>Ever since then, Somerian Health has been ensuring</p><p>effective prevention and control of COVID-19 in every</p><p>possible way. This includes diagnostic services like</p><p>RT-PCR, DPI testing, airport watch tracker programs and</p><p>antigen testing.</p><p>Headquartered in Abu Dhabi, Somerian Health &mdash; a</p><p>subsidiary of TAMOUH and the renowned IHC group of</p><p>companies &mdash; takes pride in being a leading force</p><p>throughout the pandemic by setting up 84 Covid-care</p><p>field hospitals, ICUs and clinical laboratories.</p><p>Somerian health has been organizing vaccination drives</p><p>at various touchpoints like airports, borders, churches,</p><p>gurdwaras and labour camps &ndash; only to make UAE feel</p><p>safe again</p><p><br></p><p><br></p> ";
     return Scaffold(
       appBar: CommonToolbar(title: "About Somerian Health"),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.all(60),
-              child: Image.asset(
-                "assets/images/main_logo.png",
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(60),
+                child: Image.asset(
+                  "assets/images/main_logo.png",
+                ),
               ),
             ),
-          ),
-        // Html(data: date),
-        ],
+            Html(
+                data:
+                    """<p align="justify">While the nation was fighting the virus outbreak, Somerian Health as a volunteer healthcare body came forward to join forces with the civic body.</p>
+      <p align="justify">Ever since then, Somerian Health has been ensuring effective prevention and control of COVID-19 in every possible way. This includes diagnostic services like RT-PCR, DPI testing, airport watch tracker programs and antigen testing.</p>
+      <p align="justify">Headquartered in Abu Dhabi, Somerian Health &mdash; a subsidiary of TAMOUH and the renowned IHC group of companies &mdash; takes pride in being a leading force field hospitals, ICUs and clinical laboratories.</p>
+      <p align="justify">Somerian health has been organizing vaccination drives at various touchpoints like airports, borders, churches, gurdwaras and labour camps &ndash; only to make UAE feel safe again</p>
+      """),
+          ],
+        ),
       ),
     );
   }
