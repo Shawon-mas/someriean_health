c4ffc75392743b5384f917427b7643f03b8af9fc
Mahabub Alam Shawon <mahabub15-8580@diu.edu.bd>
2022-12-22 12:58:45 +0600

    file check

--- lib/view/screens/home_screens/medical_history/hospital_records.dart
+++ lib/view/screens/home_screens/medical_history/hospital_records.dart
@@ -1,34 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../../global/properties.dart';
-
 import '../../../../model/medical_history.dart';
-import '../../../../model/vaccination-model.dart';
 import '../../../widget/common_toolbar.dart';
 import '../../../widget/custom_container.dart';
 import '../../../widget/text_widget.dart';
 import '../reports/reports.dart';
 
-
 class MEDICALRECORDS extends StatelessWidget {
-  const MEDICALRECORDS({Key? key, }) : super(key: key);
+  const MEDICALRECORDS({
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-      appBar:  CommonToolbar(title: "Hospital Records"),
+      appBar: CommonToolbar(title: "Hospital Records"),
       body: Column(
         children: [
           CustomContainer(value: "All Records"),
-          SizedBox(height: 20.h,),
+          SizedBox(
+            height: 20.h,
+          ),
           Expanded(
             child: Padding(
               padding: const EdgeInsets.all(10.0),
               child: ListView.builder(
                   itemCount: recordsList.length,
-                  itemBuilder: (context,index)
-                  {
+                  itemBuilder: (context, index) {
                     return Container(
                         width: double.infinity,
                         margin: EdgeInsets.all(10),
@@ -46,20 +45,20 @@ class MEDICALRECORDS extends StatelessWidget {
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Expanded(
-                                    flex:1,
+                                    flex: 1,
                                     child: TextWidget(
                                       edgeInsetsGeometry: EdgeInsets.all(5),
-                                      value:recordsList[index].date.toString(),
+                                      value: recordsList[index].date.toString(),
                                       size: 12.sp,
                                       fontWeight: FontWeight.w500,
                                       textColor: Colors.white,
                                     ),
                                   ),
                                   Expanded(
-                                    flex:1,
+                                    flex: 1,
                                     child: TextWidget(
                                       edgeInsetsGeometry: EdgeInsets.all(5),
-                                      value:recordsList[index].name.toString(),
+                                      value: recordsList[index].name.toString(),
                                       size: 14.sp,
                                       fontWeight: FontWeight.bold,
                                       textColor: Colors.white,
@@ -69,37 +68,45 @@ class MEDICALRECORDS extends StatelessWidget {
                               ),
                             ),
                             Row(
-
                               children: [
                                 Expanded(
-                                  flex:10,
+                                  flex: 10,
                                   child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
                                     children: [
                                       TextWidget(
                                         edgeInsetsGeometry: EdgeInsets.all(3),
-                                        value:recordsList[index].problem.toString(),
+                                        value: recordsList[index]
+                                            .problem
+                                            .toString(),
                                         size: 14.sp,
                                         fontWeight: FontWeight.w700,
                                         textColor: Properties.colorTextBlue,
                                       ),
                                       TextWidget(
                                         edgeInsetsGeometry: EdgeInsets.all(3),
-                                        value:recordsList[index].schedule.toString(),
+                                        value: recordsList[index]
+                                            .schedule
+                                            .toString(),
                                         size: 12.sp,
                                         fontWeight: FontWeight.w500,
                                         textColor: Properties.colorTextBlue,
                                       ),
                                       TextWidget(
                                         edgeInsetsGeometry: EdgeInsets.all(3),
-                                        value:recordsList[index].doctor.toString(),
+                                        value: recordsList[index]
+                                            .doctor
+                                            .toString(),
                                         size: 12.sp,
                                         fontWeight: FontWeight.w500,
                                         textColor: Properties.colorTextBlue,
                                       ),
                                       TextWidget(
                                         edgeInsetsGeometry: EdgeInsets.all(3),
-                                        value:recordsList[index].location.toString(),
+                                        value: recordsList[index]
+                                            .location
+                                            .toString(),
                                         size: 12.sp,
                                         fontWeight: FontWeight.w500,
                                         textColor: Properties.colorTextBlue,
@@ -107,42 +114,38 @@ class MEDICALRECORDS extends StatelessWidget {
                                     ],
                                   ),
                                 ),
+                                Expanded(
+                                    flex: 5,
+                                    child: InkWell(
+                                      onTap: (){
+                                        Navigator.push(context, MaterialPageRoute(builder: (context)=>MyReports(title: 'Reports',)));
 
-                                InkWell(
-                                  onTap: (){
-                                     Navigator.push(context, MaterialPageRoute(builder: (context)=>MyReports(title: 'Reports',)));
-
-                                  },
-                                  child: Expanded(
-                                      flex:5,
+                                      },
                                       child: Row(
-                                    children: [
-                                      TextWidget(
-                                        edgeInsetsGeometry: EdgeInsets.zero,
-                                        value:"View Report",
-                                        size: 12.sp,
-                                        fontWeight: FontWeight.bold,
-                                        textColor: Properties.primaryColor,
+                                        children: [
+                                          TextWidget(
+                                            edgeInsetsGeometry: EdgeInsets.zero,
+                                            value: "View Report",
+                                            size: 12.sp,
+                                            fontWeight: FontWeight.bold,
+                                            textColor: Properties.primaryColor,
+                                          ),
+                                          Icon(
+                                            Icons.arrow_forward_ios_rounded,
+                                            color: Properties.primaryColor,
+                                          )
+                                        ],
                                       ),
-                                      Icon(Icons.arrow_forward_ios_rounded,color:  Properties.primaryColor,)
-                                    ],
-                                  )),
-                                )
-
-
+                                    ))
                               ],
                             ),
-
                           ],
                         ));
-
                   }),
             ),
           ),
         ],
       ),
-
-
     );
   }
 }
--- lib/view/screens/menu_screen/my_profile/document.dart
+++ lib/view/screens/menu_screen/my_profile/document.dart
@@ -1,15 +1,34 @@
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
+import '../../../../controller/doctor_appointment_controller.dart';
 import '../../../../global/properties.dart';
 import '../../../widget/general_button.dart';
 import '../../../widget/text_widget.dart';
 
-class Document extends StatelessWidget {
+class Document extends StatefulWidget {
   const Document({Key? key}) : super(key: key);
 
   @override
+  State<Document> createState() => _DocumentState();
+}
+
+class _DocumentState extends State<Document> {
+   PlatformFile? platformFile;
+   Future selectFile() async{
+     final result=await FilePicker.platform.pickFiles();
+     if(result==null) return ;
+     setState(() {
+       platformFile=result.files.first;
+     });
+   }
+  @override
   Widget build(BuildContext context) {
+    final _controller = Get.put(DoctorAppointmentController());
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 10),
       child: Column(
@@ -17,7 +36,8 @@ class Document extends StatelessWidget {
 
           Row(
             children: [
-              Expanded(child: TextWidget(
+              Expanded(
+                  child: TextWidget(
                 value: 'Emirates ID',
                 size: 16.sp,
                 fontWeight: FontWeight.bold,
@@ -25,12 +45,15 @@ class Document extends StatelessWidget {
               )),
               Expanded(child: Column(
                 children: [
-                  AppointmentButton(
-                    onPressed: (){
 
+                 platformFile==null?
+                 AppointmentButton(
+                    onPressed: (){
+                      selectFile();
                     },
                     value: 'Upload Front Copy',
-                  ),
+                  )
+                  :Text(platformFile!.name),
                   AppointmentButton(
                     onPressed: (){
 
@@ -135,3 +158,30 @@ class Document extends StatelessWidget {
     );
   }
 }
+/*
+  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.grey,
+                      ),
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(10),
+                      ),
+                    ),
+                    child: ListTile(
+                      dense: false,
+                      contentPadding: EdgeInsets.zero,
+                      leading: IconButton(
+                        icon: Icon(Icons.attach_file_outlined),
+                        onPressed: () {
+                          _controller.filePicker();
+                        },
+                      ),
+                      title: Obx(
+                            () => Text((_controller.selectedFile.value),
+                        ),
+                      ),
+                    ),
+                  ),
+ */
