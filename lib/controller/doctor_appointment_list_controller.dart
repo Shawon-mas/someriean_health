c003fa5c263f74894a3ede745d15146d843d6ade
Mahabub Alam Shawon <mahabub15-8580@diu.edu.bd>
2023-01-05 12:40:39 +0600

    doctor appointment section and health packages appointment list update and api  services class

---
+++ lib/controller/doctor_appointment_list_controller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class DoctorAppointmentListController extends GetxController{
+  var selectIndex = 0.obs;
+  var selectStatus = 0.obs;
+  List<String> list = ["Pending", "Confirmed"];
+
+}
--- lib/view/screens/home_screens/manage_appiontment/doctor_appointment_list.dart
+++ lib/view/screens/home_screens/manage_appiontment/doctor_appointment_list.dart
@@ -1,13 +1,81 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import '../../../../controller/doctor_appointment_list_controller.dart';
+import '../../../../global/properties.dart';
+import '../../../widget/text_widget.dart';
 class DoctorAppointmentList extends StatelessWidget {
   const DoctorAppointmentList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final DoctorAppointmentListController controller=Get.put(DoctorAppointmentListController());
     return Scaffold(
-      body: Center(
-        child: Text('Doctor Appointment List'),
-      ),
-    );
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+             // margin: EdgeInsets.symmetric(horizontal: 10,vertical: 10),
+              height: 40.h,
+              child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              itemCount: controller.list.length,
+              itemBuilder: (context, index){
+                return Obx(() => InkWell(
+                  onTap: (){
+                    controller.selectIndex.value=index;
+                    controller.selectStatus.value=index;
+                  },
+                  child: Row(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Container(
+                          height: 100,
+                          decoration: BoxDecoration(
+                              boxShadow: [
+                                BoxShadow(
+                                    blurRadius: 05,
+                                    color: Colors.black12,
+                                    offset: Offset(2, 2))
+                              ],
+                              color: controller.selectStatus.value == index
+                                  ? Properties.primaryColor
+                                  : Colors.grey,
+                              borderRadius: BorderRadius.circular(10)),
+                          child: Padding(
+                              padding: EdgeInsets.all(8),
+                              child: Center(
+                                child: TextWidget(
+                                  edgeInsetsGeometry: EdgeInsets.zero,
+                                  value: controller.list[index],
+                                  textColor: controller.selectStatus.value == index
+                                      ? Colors.white
+                                      : Colors.black,
+
+                                  size: 16.sp,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                        ),
+                        ),
+                      )
+                    ],
+                  ),
+                ));
+    },
+              ),
+            ),
+           /* Obx(() => controller.selectIndex.value==0
+                ?Center(child: Text('Showing Pending List'),)
+                :Center(child: Text('Showing Confirmed List'),)
+            )*/
+          ],
+        ),
+      )
+      );
   }
 }
