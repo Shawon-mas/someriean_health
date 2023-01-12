a917ea7af8550950e3b4e009a19beee3c15cde23
Mahabub Alam Shawon <mahabub15-8580@diu.edu.bd>
2023-01-12 12:13:22 +0600

    :bug:doctor menus bug fix

--- lib/view/screens/home_screens/doctors_menu_screens/doctors_list.dart
+++ lib/view/screens/home_screens/doctors_menu_screens/doctors_list.dart
@@ -16,7 +16,6 @@ import '../../../widget/general_button.dart';
 import '../../../widget/text_widget.dart';
 
 class DoctorList extends StatefulWidget {
-
   DoctorList({Key? key}) : super(key: key);
 
   @override
@@ -28,185 +27,197 @@ class _DoctorListState extends State<DoctorList> {
   String name = "";
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CommonToolbar(title: 'Doctors'),
-      body: Column(
-        children: [
-          Container(
-            color: Colors.grey.shade300,
-            //height: 100.h,
-            width: double.infinity,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Row(
-                    children: [
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(10))),
-                          child: Obx(
-                            () => DropdownButton(
-                              hint: Text('Location'),
-                              isExpanded: true,
-                              underline: const SizedBox(),
-                              value: _controller.selectedLocation.value == ""
-                                  ? null
-                                  : _controller.selectedLocation.value,
-                              icon: const Icon(Icons.keyboard_arrow_down),
-                              items: _controller.locations.map((items) {
-                                return DropdownMenuItem(
-                                  value:
-                                      items!.hospitalLocationName!.toString(),
-                                  child: Text(
-                                      items.hospitalLocationName!.toString()),
-                                );
-                              }).toList(),
-                              onChanged: (newValue) {
-                                _controller.selectedLocation.value =
-                                    newValue as String;
-                              },
+    return WillPopScope(
+      onWillPop: ()async{
+        _controller.selectedLocation.value = "";
+        _controller.selectedSpeciality.value = "";
+        return true;
+      },
+      child: Scaffold(
+        appBar: CommonToolbar(title: 'Doctors'),
+        body: Column(
+          children: [
+            Container(
+              color: Colors.grey.shade300,
+              //height: 100.h,
+              width: double.infinity,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(10))),
+                            child: Obx(
+                              () => DropdownButton(
+                                hint: Text('Location'),
+                                isExpanded: true,
+                                underline: const SizedBox(),
+                                value: _controller.selectedLocation.value == ""
+                                    ? null
+                                    : _controller.selectedLocation.value,
+                                icon: const Icon(Icons.keyboard_arrow_down),
+                                items: _controller.locations.map((items) {
+                                  return DropdownMenuItem(
+                                    value:
+                                        items!.hospitalLocationName!.toString(),
+                                    child: Text(
+                                        items.hospitalLocationName!.toString()),
+                                  );
+                                }).toList(),
+                                onChanged: (newValue) {
+                                  _controller.selectedLocation.value =
+                                      newValue.toString();
+                                },
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        width: 10.w,
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Container(
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(10))),
-                          child: Obx(
-                            () => DropdownButton(
-                              hint: Text('Specialty'),
-                              underline: const SizedBox(),
-                              isExpanded: true,
-                              value: _controller.selectedSpeciality.value == ""
-                                  ? null
-                                  : _controller.selectedSpeciality.value,
-                              icon: const Icon(Icons.keyboard_arrow_down),
-                              items: _controller.speciality.map((items) {
-                                return DropdownMenuItem(
-                                  value:
-                                      items!.doctorSpecialitiesName!.toString(),
-                                  child: Text(
-                                      items.doctorSpecialitiesName!.toString()),
-                                );
-                              }).toList(),
-                              onChanged: (newValue) {
-                                _controller.selectedSpeciality.value =
-                                    newValue as String;
-                              },
+                        SizedBox(
+                          width: 10.w,
+                        ),
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            padding: const EdgeInsets.symmetric(horizontal: 10),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius:
+                                    const BorderRadius.all(Radius.circular(10))),
+                            child: Obx(
+                              () => DropdownButton(
+                                hint: Text('Specialty'),
+                                underline: const SizedBox(),
+                                isExpanded: true,
+                                value: _controller.selectedSpeciality.value == ""
+                                    ? null
+                                    : _controller.selectedSpeciality.value,
+                                icon: const Icon(Icons.keyboard_arrow_down),
+                                items: _controller.speciality.map((items) {
+                                  return DropdownMenuItem(
+                                    value:
+                                        items!.doctorSpecialitiesName!.toString(),
+                                    child: Text(
+                                        items.doctorSpecialitiesName!.toString()),
+                                  );
+                                }).toList(),
+                                onChanged: (newValue) {
+                                  _controller.selectedSpeciality.value =
+                                      newValue as String;
+                                },
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10.h,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(),
-                    child: TextField(
-                      //  controller: searchController,
-                      onChanged: (value) {
-                       // _controller.name = value as RxString;
-                        setState(() {
-                          name=value;
-                        });
-                      },
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          prefixIcon: Icon(Icons.search,
-                              color: Properties.colorTextBlue),
-                          hintText: "Search",
-                          hintStyle: TextStyle(
-                              fontSize: 18.sp, color: Properties.colorTextBlue),
-                          contentPadding:
-                              EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-                          focusedBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(color: Colors.white)),
-                          enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(color: Colors.white)),
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              borderSide: BorderSide(color: Colors.white))),
+                      ],
                     ),
-                  ),
-                ],
+                    SizedBox(
+                      height: 10.h,
+                    ),
+                    Container(
+                      decoration: BoxDecoration(),
+                      child: TextField(
+                        //  controller: searchController,
+                        onChanged: (value) {
+                          // _controller.name = value as RxString;
+                          setState(() {
+                            name = value;
+                          });
+                        },
+                        keyboardType: TextInputType.text,
+                        decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.white,
+                            prefixIcon: Icon(Icons.search,
+                                color: Properties.colorTextBlue),
+                            hintText: "Search",
+                            hintStyle: TextStyle(
+                                fontSize: 18.sp, color: Properties.colorTextBlue),
+                            contentPadding:
+                                EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                            focusedBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide(color: Colors.white)),
+                            enabledBorder: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide(color: Colors.white)),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide(color: Colors.white))),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 10.h,
-          ),
-          Obx(() => _controller.dataFetch.value == true
-              ? Expanded(
-                  child: ListView.builder(
-                      itemCount: _controller.doctorList.length,
-                      itemBuilder: (context, index) {
-                        List<LocationDatum?> locationData =
-                            _controller.locations;
-                        String location =
-                            locationData[index]!.hospitalLocationName!;
+            SizedBox(
+              height: 10.h,
+            ),
+            Obx(() => _controller.dataFetch.value == true
+                ? Expanded(
+                    child: ListView.builder(
+                        itemCount: _controller.doctorList.length,
+                        itemBuilder: (context, index) {
+                          List<DoctorData?> doctorListData = _controller.doctorList;
 
-                        List<Datum?> specialityData = _controller.speciality;
-                        String speciality = specialityData[index]!.doctorSpecialitiesName!;
+                         /* List<LocationDatum?> locationData = _controller.locations;
+                          String location = locationData[index]!.hospitalLocationName!.toString();
 
-                        List<DoctorData?> doctorListData = _controller.doctorList;
-
-                        if(name.isEmpty){
-                          return Obx(() => (_controller.selectedLocation.value ==
-                              "" &&
-                              _controller.selectedSpeciality.value == "")
-                              ? doctorList(doctorListData[index], context)
-                              : Visibility(
-                              visible: _controller.selectedLocation.value ==
-                                  location &&
-                                  _controller.selectedSpeciality.value ==
-                                      speciality,
-                              child: doctorList(
-                                  doctorListData[index], context)));
-                        }else if(
-                        doctorListData[index]!.doctorProfileName!.toString().toLowerCase().contains(name.toLowerCase()) ||
-                            doctorListData[index]!.doctorProfileHospitalLocationName!.toString().toLowerCase().contains(name.toLowerCase())
-                        ){
-                          return Obx(() => (_controller.selectedLocation.value ==
-                              "" &&
-                              _controller.selectedSpeciality.value == "")
-                              ? doctorList(doctorListData[index], context)
-                              : Visibility(
-                              visible: _controller.selectedLocation.value ==
-                                  location &&
-                                  _controller.selectedSpeciality.value ==
-                                      speciality,
-                              child: doctorList(
-                                  doctorListData[index], context)));
-                        }else{
-                          return Container();
-                        }
+                          List<Datum?> specialityData = _controller.speciality;
+                          String speciality =
+                              specialityData[index]!.doctorSpecialitiesName!;*/
 
 
-                      }))
-              : Center(
-                  child: CircularProgressIndicator(),
-                )),
-        ],
+
+                          if (name.isEmpty) {
+                            return Obx(() => (_controller.selectedLocation.value == "" &&
+                                    _controller.selectedSpeciality.value == "")
+                                ? doctorList(doctorListData[index], context)
+                                : Visibility(
+                                    visible: _controller.selectedLocation.value ==
+                                        doctorListData[index]!.doctorProfileHospitalLocationName! &&
+                                        _controller.selectedSpeciality.value ==
+                                            doctorListData[index]!.doctorProfileSpecialitiesName!,
+                                    child: doctorList(
+                                        doctorListData[index], context)));
+                          } else if (doctorListData[index]!
+                                  .doctorProfileName!
+                                  .toString()
+                                  .toLowerCase()
+                                  .contains(name.toLowerCase()) ||
+                              doctorListData[index]!
+                                  .doctorProfileHospitalLocationName!
+                                  .toString()
+                                  .toLowerCase()
+                                  .contains(name.toLowerCase())) {
+                            return Obx(() => (_controller
+                                            .selectedLocation.value ==
+                                        "" &&
+                                    _controller.selectedSpeciality.value == "")
+                                ? doctorList(doctorListData[index], context)
+                                : Visibility(
+                                    visible:_controller.selectedLocation.value ==
+                                        doctorListData[index]!.doctorProfileHospitalLocationName! &&
+                                        _controller.selectedSpeciality.value ==
+                                            doctorListData[index]!.doctorProfileSpecialitiesName!,
+                                    child: doctorList(
+                                        doctorListData[index], context)));
+                          } else {
+                            return Container();
+                          }
+                        }))
+                : Center(
+                    child: CircularProgressIndicator(),
+                  )),
+          ],
+        ),
       ),
     );
   }
@@ -264,6 +275,8 @@ class _DoctorListState extends State<DoctorList> {
           ),
           AppointmentButton(
             onPressed: () {
+
+
               /* _controller.selectedDoctor = SelectedDoctorModel(
                 uid: documentSnapshot.id,
                 name: documentSnapshot['name'],
